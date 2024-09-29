c0ebb147721bcd6fe968a507ed28d841ed8d8c25
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2024-09-29 19:53:13 +0700

    [FEATURE] Migration schema with MySQL GORM and DUMP database

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -5,9 +5,10 @@ import (
 	"time"
 
 	"github.com/hiumx/go-ecommerce-backend-api/global"
-	"github.com/hiumx/go-ecommerce-backend-api/internal/po"
+	"github.com/hiumx/go-ecommerce-backend-api/internal/model"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
@@ -31,6 +32,7 @@ func InitMySQL() {
 
 	SetPool()
 	migrateTable()
+	generateTableDAO()
 }
 
 func SetPool() {
@@ -47,10 +49,31 @@ func SetPool() {
 
 }
 
+func generateTableDAO() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/models",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	// gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(global.Mdb) // reuse your gorm db
+
+	// Generate basic type-safe DAO API for struct `model.User` following conventions
+	//   g.ApplyBasic(model.User{})
+	g.GenerateModel("go_crm_user")
+
+	// Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	//   g.ApplyInterface(func(Querier){}, model.User{}, model.Company{})
+
+	// Generate the code
+	g.Execute()
+}
+
 func migrateTable() {
 	err := global.Mdb.AutoMigrate(
-		&po.User{},
-		&po.Role{},
+		// &po.User{},
+		// &po.Role{},
+		&model.GoCrmUser{},
 	)
 
 	if err != nil {
